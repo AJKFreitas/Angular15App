cc98fdf12ce1f8211db961f5abaac703d8f340ba
ThiagoMarques <thiagossmarques@gmail.com>
2023-01-15 20:25:32 -0300

    feat: insere Angular Material a aplicação

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { StandAloneBootComponent } from './app/stand-alone-boot/stand-alone-boot.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 
 // platformBrowserDynamic().bootstrapModule(AppModule)
 //   .catch(err => console.error(err));
 
-bootstrapApplication(StandAloneBootComponent)
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+bootstrapApplication(StandAloneBootComponent, {
+    providers: [importProvidersFrom(BrowserAnimationsModule)]
+})
